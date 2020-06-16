8329474f444d9621880da97bc2d2deb671b75893
rybalchenko.dn <dmitrii.rybalchenko@domru.ru>
2020-06-16 12:46:12 +0500

    add build

---
+++ precache-manifest.054f9cd99c24c2435d35352dadd98140.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4aba112393ac85cf5bb2afdd7f8ac68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51ccb12a871e93ca5567",
+    "url": "/static/css/main.d5d8a3d2.chunk.css"
+  },
+  {
+    "revision": "7513f4ad41b0c10fbec1",
+    "url": "/static/js/2.632878db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.632878db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ccb12a871e93ca5567",
+    "url": "/static/js/main.939706cb.chunk.js"
+  },
+  {
+    "revision": "68dc97e49bb4ec91f87f",
+    "url": "/static/js/runtime-main.0e49a008.js"
+  },
+  {
+    "revision": "05de2776f1794b0966b239d1ec4a3b6d",
+    "url": "/static/media/FiraCode-Regular.05de2776.woff2"
+  },
+  {
+    "revision": "0ca094e965bdda43aa1beb276ed56c97",
+    "url": "/static/media/font-one-dot.0ca094e9.woff2"
+  },
+  {
+    "revision": "8f9c9ecdcf34868bad085fcc40d00b22",
+    "url": "/static/media/icon-base-screen.8f9c9ecd.svg"
+  },
+  {
+    "revision": "ce051b7baab41f670dbc508d858a87c8",
+    "url": "/static/media/icon-cross.ce051b7b.svg"
+  },
+  {
+    "revision": "233fdc4a0113c26f5f44a7b35218c03d",
+    "url": "/static/media/icon-format.233fdc4a.svg"
+  },
+  {
+    "revision": "8b327a5d96ff22bd93a8133efad04ec1",
+    "url": "/static/media/icon-full-screen.8b327a5d.svg"
+  },
+  {
+    "revision": "6fb60e45ff3a63ab6cf6c05a9bf7b2ef",
+    "url": "/static/media/icon-logout.6fb60e45.svg"
+  }
+]);
